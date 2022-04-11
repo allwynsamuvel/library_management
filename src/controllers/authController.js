3ca981c76a0e7d62c5e028c7e9b9da58a7446c6a
Allwyn <allwyns@axioned.com>
2022-04-11 15:56:27 +0530

    login function added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,52 +1,80 @@
-const authService = require('../services/authServices');
-const errorMsg = require('../helpers/errorMessage').errorMessages;
-const utils = require('../helpers/utils');
+const authService = require("../services/authServices");
+const errorMsg = require("../helpers/errorMessage").errorMessages;
+const utils = require("../helpers/utils");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { joiSchema } = require("../helpers/joi_validation");
 
 /**
+ * @description Local login controller.
+ * @function login
+ */
+exports.login = async (req, res) => {
+  try {
+    const data = await mongoose
+      .model("user")
+      .findOne({ email: req.body.email });
+    if (data) {
+      const result = await bcrypt.compare(req.body.password, data.password);
+      if (result) {
+        const user = { email: data.email, role: data.role };
+        const accessToken = authService.createToken(user);
+        const msgObj = {
+          message: `login success (${data.name} as ${data.role})`,
+          token: accessToken,
+        };
+        return res.send(utils.responseMsg(null, true, msgObj));
+      }
+    }
+    const msg = "Login failed";
+    return res.status(401).send(utils.responseMsg(null, true, msg));
+  } catch (err) {
+    console.log("ERROR", err.stack);
+    return res
+      .status(500)
+      .send(utils.responseMsg(errorMsg.internalServerError));
+  }
+};
+
+/**
  * @description Local signup controller.
  * @function signup
  */
- exports.signup = async (req, res) => {
+exports.signup = async (req, res) => {
   try {
     const doc = await joiSchema.validateAsync(req.body);
-    if(doc.error) throw doc.error;
+    if (doc.error) throw doc.error;
 
     const bearerHeader = req.headers["authorization"];
-    if (bearerHeader) { 
+    if (bearerHeader) {
       const token = bearerHeader.split(" ")[1];
-  
+
       const token_data = jwt.verify(token, process.env.JWT_SECRET);
       req.user = token_data;
     }
 
-    if(req.user != undefined && req.user.role == "customer") {
+    if (req.user != undefined && req.user.role == "customer") {
       const msg = {
         error: "unauthorized",
-        message: "Customer not allowed create other user"
-      }
+        message: "Customer not allowed to create other user",
+      };
       return res.status(401).send(utils.responseMsg(true, false, msg));
     }
 
     doc.role = doc.role.toLowerCase();
     if (doc.role == "admin") {
-      if(req.user == undefined || req.user.role != "admin") { 
+      if (req.user == undefined || req.user.role != "admin") {
         const msg = {
           error: "unauthorized",
-          message: "Only Admin assign user role as 'Admin'."
-        }
+          message: "Only Admin assign user role as 'Admin'.",
+        };
         return res.status(401).send(utils.responseMsg(true, false, msg));
       }
     }
 
     const userExist = await mongoose.model("user").findOne({
-      $or: [
-        { email: doc.email },
-        { phone: doc.phone },
-      ],
+      $or: [{ email: doc.email }, { phone: doc.phone }],
     });
     if (userExist == null) {
       doc.password = await bcrypt.hash(doc.password, 10);
@@ -68,7 +96,8 @@ const { joiSchema } = require("../helpers/joi_validation");
       return res.status(409).send(utils.responseMsg(true, false, msg));
     }
   } catch (err) {
-    if(err.isJoi) return res.status(422).send(utils.responseMsg(true, false, err.details));
+    if (err.isJoi)
+      return res.status(422).send(utils.responseMsg(true, false, err.details));
     console.error("ERROR", err.stack);
     return res.status(500).send(utils.responseMsg(true, false, err.errors));
   }
