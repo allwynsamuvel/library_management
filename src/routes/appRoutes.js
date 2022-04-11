9eef2d02658d60324b69818465e85405395fda0d
Allwyn <allwyns@axioned.com>
2022-04-11 18:34:27 +0530

    admin routes and costomer routes are added

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
-//const dependencies = require('./routesDependencies').default;
+const { appClient } = require('./routesDependencies').default;
+const { tokenVerify } = require("../helpers/utils");
+
+// customer routes
+router.get('/users', tokenVerify("customer"), appClient.userProfile);
+router.put('/users', tokenVerify("customer"), appClient.userUpdate);
+router.delete('/users', tokenVerify("customer"), appClient.userDelete);
+
+// admin routes 
+router.get("/admin/users/:id", tokenVerify("admin"), appClient.userProfile);
+router.put("/admin/users/:id", tokenVerify("admin"), appClient.userUpdate);
+router.delete("/admin/users/:id", tokenVerify("admin"), appClient.userDelete);
+
 
 module.exports = router;
