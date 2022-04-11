72526a077d3054d59a58a52c1453e65d036124de
Allwyn <allwyns@axioned.com>
2022-04-11 18:41:45 +0530

    user profile update controller added

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -38,3 +38,50 @@ exports.userProfile = async (req, res) => {
   }
 };
 
+/**
+ * @description userUpdate controller.
+ * @function userUpdate
+ */
+exports.userUpdate = async (req, res) => {
+  try {
+    const doc = await joiSchema.validateAsync(req.body);
+    if(doc.error) throw doc.error;
+    
+    const userId = req.user.userId;
+    const userData = await mongoose.model("user").findOne({ _id: ObjectId(userId) });
+    if (userData) {
+        const password_exist = doc.hasOwnProperty("password");
+        if (req.params.id != undefined && req.user.role == "admin") {
+            if (password_exist) {
+                const hash = await bcrypt.hash(doc.password, 10);
+                doc.password = hash;
+                const a = await mongoose.model("user").updateOne({ _id: ObjectId(req.params.id) }, { $set: doc });
+                console.log("err",a); 
+            }
+            await mongoose
+            .model("user")
+            .updateOne({ _id: ObjectId(req.params.id) }, { $set: doc });
+
+            const msgData = "Updated";
+            return res.send(utils.responseMsg(false, true, msgData));
+        }
+
+      
+        if (password_exist) {
+            const hash = await bcrypt.hash(doc.password, 10);
+            doc.password = hash;
+            await mongoose.model("user").updateOne({ _id: ObjectId(userId) }, { $set: doc });
+        } else await mongoose.model("user").updateOne({ _id: ObjectId(userId) }, { $set: doc });
+
+        const msg = "Updated";
+        return res.send(utils.responseMsg(false, true, msg));
+    }
+    return res.status(401).send(utils.responseMsg(errorMsg.unauthorized));
+  } catch (err) {
+    console.log("ERROR", err.stack);
+    return res
+      .status(400)
+      .send(utils.responseMsg(true, false, err));
+  }
+};
+
