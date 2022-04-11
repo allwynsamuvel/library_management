9191fb729477d845651bff99f068487fa75d9ec8
Allwyn <allwyns@axioned.com>
2022-04-11 15:25:40 +0530

    signup routes added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,41 +1,7 @@
 const router = require('express').Router();
-const dependencies = require('./routesDependencies').default;
+const { authClient } = require('./routesDependencies').default;
 
-/**
- * @swagger
- * /auth/login:
- *  post:
- *    tags:
- *      - Authentication
- *    name: Local Login API
- *    summary: Based on user's data, this api sent jwt token which leads to login process.
- *    consumes:
- *      - application/json
- *    produces:
- *      - application/json
- *    parameters:
- *      - name: Body Data
- *        in: body
- *        schema:
- *         type: object
- *         properties:
- *          email: 
- *            type: string
- *          password:
- *            type: string
- *        required:
- *         - email
- *         - password
- *    responses:
- *      200:
- *        description: JWT token will be in response.
- *      500:
- *        description: Internal server error.
- */
-router.post('/login', dependencies.authClient.login);
-
-/**
- * @note All routes regarding local signup OR using Oauth sign-in should be listed below. 
- */
+router.post('/login', authClient.login);
+router.post('/signup', authClient.signup);
 
 module.exports = router;
