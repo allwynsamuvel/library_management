d267ddb9333d423ba8b303432b2e18b1c7c87d55
Allwyn <allwyns@axioned.com>
2022-04-12 14:18:16 +0530

    remove unused code

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,33 +4,18 @@ const user_schema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    match: [/^[a-zA-Z, ]+$/, "Enter Valid Name"],
     trim: true,
   },
   phone: {
     type: Number,
     unique: true,
     required: true,
-    validate: [
-        (e) => {
-          const phone_pattern = /^\d{10}$/;
-          return phone_pattern.test(e);
-        },
-        "Enter Valid Phone",
-    ],
   },
   email: {
     type: String,
     lowercase: true,
     unique: true,
     required: true,
-    validate: [
-      (e) => {
-        const email_pattern = /.+\@.+\..+/;
-        return email_pattern.test(e);
-      },
-      "Enter Valid Email Address",
-    ],
   },
   password: {
     type: String,
