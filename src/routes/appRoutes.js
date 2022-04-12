70009b5818ee2d90cdd4155b560bffa6ea72cdb7
Allwyn <allwyns@axioned.com>
2022-04-12 16:58:10 +0530

    comment added for route group

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,21 +1,22 @@
-const router = require('express').Router();
-const { appClient, authClient } = require('./routesDependencies').default;
+const router = require("express").Router();
+const { appClient, authClient } = require("./routesDependencies").default;
 const { tokenVerify } = require("../helpers/utils");
 
 router.get("/users", tokenVerify("customer"), appClient.userProfile);
 router.put("/users", tokenVerify("customer"), appClient.userUpdate);
 router.delete("/users", tokenVerify("customer"), appClient.userDelete);
 
+// search routes
 router.get("/books/:option/:searchQuery", tokenVerify("customer"), appClient.bookSearch);
 
-// admin routes 
+// admin routes
 router.get("/admin/users/:id", tokenVerify("admin"), appClient.userProfile);
 router.post("/admin/users", tokenVerify("admin"), authClient.signup);
 router.put("/admin/users/:id", tokenVerify("admin"), appClient.userUpdate);
 
+// books routes
 router.post("/admin/books", tokenVerify("admin"), appClient.addBook);
 router.put("/admin/books/:id", tokenVerify("admin"), appClient.updateBook);
 router.delete("/admin/books/:id", tokenVerify("admin"), appClient.deleteBook);
-
 
 module.exports = router;
