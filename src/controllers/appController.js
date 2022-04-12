c0a812634da1547cf2adab0cd077ede9965fbdc4
Allwyn <allwyns@axioned.com>
2022-04-12 12:04:55 +0530

    objectId removed and instead using objectId, ID used directly

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -3,7 +3,6 @@ const utils = require("../helpers/utils");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const { joiSchema } = require("../helpers/joi_validation");
-const { ObjectId } = require("bson");
 
 /**
  * @description userProfile controller.
@@ -11,12 +10,11 @@ const { ObjectId } = require("bson");
  */
 exports.userProfile = async (req, res) => {
   try {
-    //   change to req.user.role to user.id then check role using doc.
     if (req.user.role == "admin") {
       if (req.params.id != undefined) {
         const udata = await mongoose
           .model("user")
-          .findOne({ _id: ObjectId(req.params.id) });
+          .findOne({ _id: req.params.id });
         return res.send(utils.responseMsg(false, true, udata));
       }
       const data = await mongoose.model("user").find();
@@ -33,9 +31,7 @@ exports.userProfile = async (req, res) => {
     return res.status(401).send(utils.responseMsg(errorMsg.unauthorized));
   } catch (err) {
     console.log("ERROR", err.stack);
-    return res
-      .status(400)
-      .send(utils.responseMsg(true, false, err));
+    return res.status(400).send(utils.responseMsg(true, false, err));
   }
 };
 
@@ -46,45 +42,44 @@ exports.userProfile = async (req, res) => {
 exports.userUpdate = async (req, res) => {
   try {
     const doc = await joiSchema.validateAsync(req.body);
-    if(doc.error) throw doc.error;
-    
+    if (doc.error) throw doc.error;
+
     const userId = req.user.userId;
-    const userData = await mongoose.model("user").findOne({ _id: ObjectId(userId) });
+    const userData = await mongoose.model("user").findOne({ _id: userId });
     if (userData) {
-        const password_exist = doc.hasOwnProperty("password");
-        if (req.params.id != undefined && req.user.role == "admin") {
-            if (password_exist) {
-                const hash = await bcrypt.hash(doc.password, 10);
-                doc.password = hash;
-                const a = await mongoose.model("user").updateOne({ _id: ObjectId(req.params.id) }, { $set: doc });
-                console.log("err",a); 
-            }
-            await mongoose
-            .model("user")
-            .updateOne({ _id: ObjectId(req.params.id) }, { $set: doc });
-
-            const msgData = "Updated";
-            return res.send(utils.responseMsg(false, true, msgData));
-        }
-
-      
+      const password_exist = doc.hasOwnProperty("password");
+      if (req.params.id != undefined && req.user.role == "admin") {
         if (password_exist) {
-            const hash = await bcrypt.hash(doc.password, 10);
-            doc.password = hash;
-            await mongoose.model("user").updateOne({ _id: ObjectId(userId) }, { $set: doc });
-        } else await mongoose.model("user").updateOne({ _id: ObjectId(userId) }, { $set: doc });
+          const hash = await bcrypt.hash(doc.password, 10);
+          doc.password = hash;
+          await mongoose
+            .model("user")
+            .updateOne({ _id: req.params.id }, { $set: doc });
+        }
+        await mongoose
+          .model("user")
+          .updateOne({ _id: req.params.id }, { $set: doc });
 
-        const msg = "Updated";
-        return res.send(utils.responseMsg(false, true, msg));
+        const msgData = "Updated";
+        return res.send(utils.responseMsg(false, true, msgData));
+      }
+
+      if (password_exist) {
+        const hash = await bcrypt.hash(doc.password, 10);
+        doc.password = hash;
+        await mongoose.model("user").updateOne({ _id: userId }, { $set: doc });
+      } else
+        await mongoose.model("user").updateOne({ _id: userId }, { $set: doc });
+
+      const msg = "Updated";
+      return res.send(utils.responseMsg(false, true, msg));
     }
     return res.status(401).send(utils.responseMsg(errorMsg.unauthorized));
   } catch (err) {
     if (err.isJoi)
       return res.status(422).send(utils.responseMsg(true, false, err.details));
     console.log("ERROR", err.stack);
-    return res
-      .status(400)
-      .send(utils.responseMsg(true, false, err));
+    return res.status(400).send(utils.responseMsg(true, false, err));
   }
 };
 
@@ -94,29 +89,31 @@ exports.userUpdate = async (req, res) => {
  */
 exports.userDelete = async (req, res) => {
   try {
-    const userData = await mongoose.model("user").findOne({ _id: ObjectId(req.user.userId) });
-    if(userData) {
-        if(userData.role == "admin") {
-            const uData = await mongoose
-                .model("user")
-                .findOne({ _id: ObjectId(req.params.id) });
-        
-                console.log(uData);
-            if (uData != null) {
-                await mongoose.model("user").deleteOne({ _id: uData._id });
-                const msg = "User Deleted";
-                return res.send(utils.responseMsg(false, true, msg));
-            }
-            res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
-        }
+    const userData = await mongoose
+      .model("user")
+      .findOne({ _id: req.user.userId });
+    if (userData) {
+      if (userData.role == "admin") {
+        const uData = await mongoose
+          .model("user")
+          .findOne({ _id: req.params.id });
 
-        if(userData.role == "customer") {
-            await mongoose.model("user").deleteOne({ _id: req.user.userId });
-            const msg = "User Deleted";
-            return res.send(utils.responseMsg(false, true, msg));
+        console.log(uData);
+        if (uData != null) {
+          await mongoose.model("user").deleteOne({ _id: uData._id });
+          const msg = "User Deleted";
+          return res.send(utils.responseMsg(false, true, msg));
         }
-    } 
-    return res.status(401).send(utils.responseMsg(errorMsg.unauthorized)); 
+        res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
+      }
+
+      if (userData.role == "customer") {
+        await mongoose.model("user").deleteOne({ _id: req.user.userId });
+        const msg = "User Deleted";
+        return res.send(utils.responseMsg(false, true, msg));
+      }
+    }
+    return res.status(401).send(utils.responseMsg(errorMsg.unauthorized));
   } catch (err) {
     console.log("ERROR", err.stack);
     return res
@@ -129,106 +126,112 @@ exports.userDelete = async (req, res) => {
  * @description add book controller.
  * @function addBook
  */
- exports.addBook = async (req, res) => {
-   try {
-     const doc = req.body;
-     const bookData = await mongoose.model("book").findOne({ name: doc.name });
-     if(bookData == null) {
-       if(doc.bookCopies == 0) doc.availability = false;
-       const data = await mongoose.model("book").create(doc);
-  
-       const msg = {
-         message: "Book added",
-         data: data
-       };
-       return res.send(utils.responseMsg(false, true, msg));
-     } else { 
-       const obj = { message: "Book already Existed" }
-       return res.send(utils.responseMsg(true, false, obj));
-     }
-   } catch (err) {
+exports.addBook = async (req, res) => {
+  try {
+    const doc = req.body;
+    const bookData = await mongoose.model("book").findOne({ name: doc.name });
+    if (bookData == null) {
+      if (doc.bookCopies == 0) doc.availability = false;
+      const data = await mongoose.model("book").create(doc);
+
+      const msg = {
+        message: "Book added",
+        data: data,
+      };
+      return res.send(utils.responseMsg(false, true, msg));
+    } else {
+      const obj = { message: "Book already Existed" };
+      return res.send(utils.responseMsg(true, false, obj));
+    }
+  } catch (err) {
     console.log("ERROR", err.stack);
-    return res
-      .status(500)
-      .send(utils.responseMsg(true, false, err));
-   }
- };
+    return res.status(500).send(utils.responseMsg(true, false, err));
+  }
+};
 
 /**
  * @description update book controller.
  * @function updateBook
  */
- exports.updateBook = async (req, res) => {
-   try {
-     const doc = req.body;
-     const data = await mongoose.model("book").findOne({_id: req.params.id});
-     if(data != null) {
-       if(doc.bookCopies == 0) doc.availability = false;
-       await mongoose.model("book").updateOne({_id: req.params.id},{$set: doc});
-   
-       const msg = { message: "Book updated" };
-       return res.send(utils.responseMsg(false, true, msg));
-     }
-     return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
-   } catch (err) {
+exports.updateBook = async (req, res) => {
+  try {
+    const doc = req.body;
+    const data = await mongoose.model("book").findOne({ _id: req.params.id });
+    if (data != null) {
+      if (doc.bookCopies == 0) doc.availability = false;
+      await mongoose
+        .model("book")
+        .updateOne({ _id: req.params.id }, { $set: doc });
+
+      const msg = { message: "Book updated" };
+      return res.send(utils.responseMsg(false, true, msg));
+    }
+    return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
+  } catch (err) {
     console.log("ERROR", err.stack);
-    return res
-      .status(500)
-      .send(utils.responseMsg(true, false, err));
-   }
- };
+    return res.status(500).send(utils.responseMsg(true, false, err));
+  }
+};
 
 /**
  * @description delete book controller.
  * @function deleteBook
  */
- exports.deleteBook = async (req, res) => {
-   try {
-     const data =  await mongoose.model("book").findOne({_id: req.params.id});
-     if(data != null) {
-       await mongoose.model("book").deleteOne({_id: req.params.id});
+exports.deleteBook = async (req, res) => {
+  try {
+    const data = await mongoose.model("book").findOne({ _id: req.params.id });
+    if (data != null) {
+      await mongoose.model("book").deleteOne({ _id: req.params.id });
 
-       const msg = { message: "Book deleted" };
-       return res.send(utils.responseMsg(false, true, msg));
-     }
-     return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
-   } catch (err) {
+      const msg = { message: "Book deleted" };
+      return res.send(utils.responseMsg(false, true, msg));
+    }
+    return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
+  } catch (err) {
     console.log("ERROR", err.stack);
-    return res
-      .status(500)
-      .send(utils.responseMsg(true, false, err));
-   }
- };
+    return res.status(500).send(utils.responseMsg(true, false, err));
+  }
+};
 
 /**
  * @description book search controller.
  * @function bookSearch
  */
- exports.bookSearch = async (req, res) => {
-   try {
-     if(req.params.option == "name") {
-      const data = await mongoose.model("book").find({name: req.params.searchQuery});
-      if(data.length != 0) return res.send(utils.responseMsg(false, true, data)); 
-      else return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
-     }
-     
-     if(req.params.option == "author") {
-      const data = await
-       mongoose.model("book").find({author: req.params.searchQuery});
-      if(data.length != 0) return res.send(utils.responseMsg(false, true, data)); 
-      else return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
-     }
+exports.bookSearch = async (req, res) => {
+  try {
+    if (req.params.option == "name") {
+      const data = await mongoose
+        .model("book")
+        .find({ name: req.params.searchQuery });
+      if (data.length != 0)
+        return res.send(utils.responseMsg(false, true, data));
+      else
+        return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
+    }
 
-     if(req.params.option == "category") {
-      const data = await mongoose.model("book").find({category: req.params.searchQuery});
-      if(data.length != 0) return res.send(utils.responseMsg(false, true, data)); 
-      else return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
-     }
-     return res.status(401).send(utils.responseMsg(errorMsg.unauthorized));
-   } catch (err) {
+    if (req.params.option == "author") {
+      const data = await mongoose
+        .model("book")
+        .find({ author: req.params.searchQuery });
+      if (data.length != 0)
+        return res.send(utils.responseMsg(false, true, data));
+      else
+        return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
+    }
+
+    if (req.params.option == "category") {
+      const data = await mongoose
+        .model("book")
+        .find({ category: req.params.searchQuery });
+      if (data.length != 0)
+        return res.send(utils.responseMsg(false, true, data));
+      else
+        return res.status(404).send(utils.responseMsg(errorMsg.dataNotFound));
+    }
+
+    return res.status(401).send(utils.responseMsg(errorMsg.unauthorized));
+  } catch (err) {
     console.log("ERROR", err.stack);
-    return res
-      .status(500)
-      .send(utils.responseMsg(true, false, err));
-   }
- };
+    return res.status(500).send(utils.responseMsg(true, false, err));
+  }
+};
