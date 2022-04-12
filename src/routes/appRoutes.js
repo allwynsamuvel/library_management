d1e7ce8ed6daeb3d0949c6f7d4a81e357f98ecfe
Allwyn <allwyns@axioned.com>
2022-04-12 11:36:38 +0530

    books routes are added

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -2,15 +2,20 @@ const router = require('express').Router();
 const { appClient } = require('./routesDependencies').default;
 const { tokenVerify } = require("../helpers/utils");
 
-// customer routes
-router.get('/users', tokenVerify("customer"), appClient.userProfile);
-router.put('/users', tokenVerify("customer"), appClient.userUpdate);
-router.delete('/users', tokenVerify("customer"), appClient.userDelete);
+router.get("/users", tokenVerify("customer"), appClient.userProfile);
+router.put("/users", tokenVerify("customer"), appClient.userUpdate);
+router.delete("/users", tokenVerify("customer"), appClient.userDelete);
+
+router.get("/books/:option/:searchQuery", tokenVerify("customer"), appClient.bookSearch);
 
 // admin routes 
 router.get("/admin/users/:id", tokenVerify("admin"), appClient.userProfile);
 router.put("/admin/users/:id", tokenVerify("admin"), appClient.userUpdate);
 router.delete("/admin/users/:id", tokenVerify("admin"), appClient.userDelete);
+
+router.post("/admin/books", tokenVerify("admin"), appClient.addBook);
+router.put("/admin/books/:id", tokenVerify("admin"), appClient.updateBook);
+router.delete("/admin/books/:id", tokenVerify("admin"), appClient.deleteBook);
 
 
 module.exports = router;
